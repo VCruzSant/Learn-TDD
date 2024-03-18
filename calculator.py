282f60fed9490efcc7431d7455dae33edccae60e
VCruzSant <sviniantiago@gmail.com>
2024-03-18 13:06:02 -0300

    testing with doctest

--- calculator.py
+++ calculator.py
@@ -1,4 +1,29 @@
 def sum(x, y):
+    """Sum X and Y
+
+
+    Test -> OK
+    >>> sum(31, 20)
+    51
+
+    Test -> Failure
+    >>> sum(30, 20)
+    51
+
+    Test -> Failure
+    >>> sum('30', 20)
+
+    Test -> OK
+    >>> sum('30', 20)
+    Traceback (most recent call last):
+    ...
+    AssertionError: must be int or float
+    """
     assert isinstance(x, (int, float)), 'must be int or float'
     assert isinstance(y, (int, float)), 'must be int or float'
     return x + y
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
